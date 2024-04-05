4c5e0841be69c3ab8eb02a680d33a9ed1e8c263c
Junjie <72896613+DJJ0817@users.noreply.github.com>
2024-04-05 01:58:30 -0400

    Add files via upload

--- Solving A Fruits Classification Problem/Solving A Fruits Classification Problem.py
+++ Solving A Fruits Classification Problem/Solving A Fruits Classification Problem.py
@@ -102,4 +102,18 @@ def plot_fruit_knn(X, y, n_neighbors, weights):
 
 plot_fruit_knn(X, y, 5, 'uniform')
 
+"""
+### choose the best n_neighbor
+k_range = range(1, 20)
+scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors = k)
+    knn.fit(X_train, y_train)
+    scores.append(knn.score(X_test, y_test))
+plt.figure()
+plt.xlabel('k')
+plt.ylabel('accuracy')
+plt.scatter(k_range, scores)
+plt.xticks([0,5,10,15,20])
 
+"""
